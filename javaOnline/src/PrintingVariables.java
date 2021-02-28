fa47deb232b7ab9594d3aae52d40991aaa95b27f
Sanderson Ferraz <sandersonferraz@gmail.com>
2021-02-27 23:51:47 -0300

    created PrintValiable.java

---
+++ javaOnline/src/PrintingVariables.java
@@ -0,0 +1,13 @@
+public class PrintingVariables {
+    /***
+     * Description code.
+     * @param args parametro de entrada do metodo main
+     */
+    public static void main(String[] args){
+
+        int age = 10; // Integer variable type
+        int matherIsAge = 45;
+        int fatherIsAge = 49;
+        System.out.println("Your age. " + age + ", mather's age: " + matherIsAge + ", father's age: " + fatherIsAge + ".");
+    }
+}
